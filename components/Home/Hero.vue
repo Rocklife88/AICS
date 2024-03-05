5338e0f8b216e92b07788e6c1890030ef5dd8bf6
marco <pelleu2@gmail.com>
2024-03-05 17:41:07 +0100

    improved hero section

--- components/Home/Hero.vue
+++ components/Home/Hero.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="flex-reverse items-center md:flex  justify-center md:min-h-screen  min-h-full mb-44 md:mb-0  w-full mx-auto white pt-20 md:pt-2 md:mt-0 bg-white ">
+    <div class="flex-reverse items-center md:flex  justify-center md:min-h-screen  min-h-full mb-44 md:mb-0  w-full mx-auto white  md:pt-2 md:mt-0 bg-white ">
       
   <!-- Container flessibile per la hero section -->
   <div class="container  relative  gap-1 items-center mx-auto flex flex-col md:flex-row  ">
     <!-- Blocco sinistro con testo e pulsante -->
     <div class=" w-full md:w-1/2">
       <div class="text-left ">
-        <img src="/assets/images/logo_aicslucca.png" alt="">
+        <img class="w-1/3 " src="/assets/images/logo_aicslucca.png" alt="">
         <h1 class="text-4xl md:text-5xl font-bold text-primaryDarker">IL TUO SPORT<br> 
           SEMPRE CON TE</h1>
         <p class="mt-4 text-xl

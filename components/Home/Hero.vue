bba1112bbc75a1f817acbda2613fd27af9cc93a9
marco <pelleu2@gmail.com>
2024-03-06 18:04:05 +0100

    refactor and improive code

--- components/Home/Hero.vue
+++ components/Home/Hero.vue
@@ -1,32 +1,35 @@
 <template>
-  <div class="relative w-full min-h-screen overflow-hidden">
-    <!-- Video in background -->
-    <video class="absolute top-0 left-0 z-0 w-full h-full object-cover" autoplay muted loop>
-      <source src="/assets/video/calcio.mp4" type="video/mp4">
-      Your browser does not support the video tag.
-    </video>
-
-    <!-- Overlay con opacità -->
-    <div class="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div>
-
-    <!-- Container per il contenuto -->
-    <div class="relative z-10 flex items-center justify-center min-h-screen py-6 md:pb-60">
-      <div class="container mx-auto flex flex-col md:flex-row gap-1 items-center p-6 text-white text-left">
-        <!-- Blocco sinistro con testo e pulsante -->
-        <div class="w-full md:w-1/2">
-          <div>
-            <img class="w-1/3" src="/assets/images/logo_aicslucca.png" alt="" />
-            <h1 class="text-3xl md:text-4xl font-bold">
-              IL TUO SPORT<br />
-              SEMPRE CON TE
-            </h1>
-            <p class="mt-4 text-lg md:text-xl">
-              Entra nel mondo AICS e diventi protagonista del tuo sport! Contenuti
-              sempre aggiornati e un'app dedicata per darti il massimo.
-            </p>
-          </div>
+  <div
+    class="flex-reverse items-center md:flex justify-center  min-h-full pb-0 md:pb-60 w-full mx-auto white md:pt-2 md:mt-0 bg-white"
+  >
+    <!-- Container flessibile per la hero section -->
+    <div
+      class="container relative gap-1 items-center mx-auto flex flex-col md:flex-row p-6"
+    >
+      <!-- Blocco sinistro con testo e pulsante -->
+      <div class="w-full md:w-1/2">
+        <div class="text-left">
+          <img class="w-1/3" src="/assets/images/logo_aicslucca.png" alt="" />
+          <h1 class="text-3xl md:text-4xl font-bold text-primaryDarker">
+            IL TUO SPORT<br />
+            SEMPRE CON TE
+          </h1>
+          <p class="mt-4 text-lg md:text-xl text-primary">
+            Entra nel mondo AICS e diventi protagonista del tuo sport! Contenuti
+            sempre aggiornati e un app dedicata per darti il massimo
+          </p>
         </div>
+      </div>
+      <!-- Blocco destro con immagine -->
+      <div class="md:block md:w-2/3 md:mt-0">
+        <img
+          src="/assets/images/hero.png"
+          alt="Immagine squadra calcio"
+          class="w-full h-90 object-cover"
+        />
       </div>
     </div>
   </div>
 </template>
+
+
